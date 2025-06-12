a73590611b27c617db88229831c06c2821290780
kyxent-Immortal-Dev <humbertoezequiel.z.c@gmail.com>
2025-06-11 23:06:11 -0600

    fixed detail

--- reservasApp/src/main/java/com/reservas/ReservationApp.java
+++
@@ -1,9 +0,0 @@
-public class ReservationApp {
-    public static void main(String[] args) {
-        // Inicialización del sistema de reservas
-        System.out.println("Bienvenido al Sistema de Reservas en Línea");
-        
-        // Aquí se puede agregar la lógica para iniciar la aplicación,
-        // como la configuración de servicios, la interfaz de usuario, etc.
-    }
-}
